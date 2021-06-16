c90341b12c2113b6b7def9ef04848c7b351f8a5d
Daniel Montero <ing.danielmontero@gmail.com>
2021-06-15 23:09:07 -0500

    Restore ESC position

--- keyboards/lily58/keymaps/dmontero/keymap.c
+++ keyboards/lily58/keymaps/dmontero/keymap.c
@@ -15,12 +15,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | `    |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |backspace|
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |backspace|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------| ESC   |    | ENTER |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| `     |    | ENTER |------+------+------+------+------+------|
  * | + =  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LCTRL| Win  |lAlt  | /Space  /       \Space \  |QWERTY|LOWER | RAISE|
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   TD(TD_C_L),KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_EQUAL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_ESC, KC_ENT,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_MINS, \
+  KC_EQUAL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_GRV, KC_ENT,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_MINS, \
                        KC_LCTRL, KC_LGUI, KC_LALT,    KC_SPC, KC_SPC, KC_LALT, MO(_LOWER), MO(_RAISE) \
 ),
 /* LOWER
