915a33fc294bceeabbadf8949205c1aa5243cc5d
Daniel Montero Cervantes <ing.danielmontero@gmail.com>
2025-08-24 23:37:39 -0600

    Base

--- keyboards/keychron/q5/ansi/keymaps/monterokm/keymap.c
+++ keyboards/keychron/q5/ansi/keymaps/monterokm/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,        KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,             KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,              KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 MO(FN),      MO(FN),     MO(FN),      KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT,  KC_PENT),
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCTL,     KC_ROPT,     MO(FN),      KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT,  KC_PENT),
     [FN] = LAYOUT_ansi_100(
         _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,     _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,  _______,
@@ -42,9 +42,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef RGB_MATRIX_ENABLE
 
+
 bool rgb_matrix_indicators_user(void) {
-    // Apaga todo
+    // Apaga todos los LEDs
     rgb_matrix_set_color_all(0, 0, 0);
+
+    // Si la capa FN está activa, colorea todo de azul
+    if (layer_state_is(FN)) {
+        for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+            rgb_matrix_set_color(i, 60, 60, 215); // azul
+        }
+    }
 
     // Caps Lock activa → rojo
     if (host_keyboard_led_state().caps_lock) {
@@ -56,7 +64,7 @@ bool rgb_matrix_indicators_user(void) {
         rgb_matrix_set_color(NUM_LOCK_LED_INDEX, 255, 255, 255);
     }
 
-    return false; // Ignora efectos RGB predeterminados
-}
 
+    return false; // ignora efectos RGB predeterminados
+}
 #endif
